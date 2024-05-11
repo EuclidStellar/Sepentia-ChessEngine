d777375f65454efdae462b74e73d5d0f6a6d736f
euclidstellar <euclidstellar@gmail.com>
2024-05-11 23:16:29 +0530

    chess-ai

--- src/chessengine.py
+++ src/chessengine.py
@@ -4,6 +4,11 @@
 
 class GameState():
     def __init__(self):
+        #board is an 8x8 2d list, each element of the list has 2 characters
+        #the first character represents the color of the piece, 'b' or 'w'
+        #the second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N', 'P'
+        # "--" represents an empty space with no piece
+        
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -14,3 +19,6 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"], 
         ]
+        self.whiteToMove = True
+        self.moveLog = []
+        
--- src/chessmain.py
+++ src/chessmain.py
@@ -1,1 +1,84 @@
 #main driver file for chess game
+
+import pygame as p
+# from chessengine import ChessEngine
+import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 #for animations later on
+IMAGES = {}
+
+'''
+initialize a global dictionary of images. This will be called exactly once in the main
+'''
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    #Note: we can access an image by saying 'IMAGES['wp']'
+    
+'''
+The main driver for our code. This will handle user input and updating the graphics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()  
+    loadImages() #only do this once, before the while loop
+    running = True
+    sqSelected = () #no square is selected, keep track of the last click of the user (tuple: (row, col))
+    playerClicks = [] #keep track of player clicks (two tuples: [(6, 4), (4, 4)])
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col= location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+    
+'''
+Responsible for all the graphics within a current game state
+
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw squares on the board
+    drawPieces(screen, gs.board) #draw pieces on top of those squares
+    
+'''
+Draw the squares on the board. The top left square is always light'''
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            
+    '''
+    Draw the pieces on the board using the current GameState.board'''        
+            
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                
+if __name__ == "__main__":
+    main()
